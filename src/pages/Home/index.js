0026d5260eff397e9e0f3726b79284eb38129eed
Silviane Bezerra <silvianeraquel@gmail.com>
2021-12-28 14:14:07 -0300

    API e Requisição HTTP  da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 export default function Home() {
+  const [filmes, setfilmes] = useState([]);
+
+  useEffect(()=>{
+    async function loadFilmes(){
+      const response = await api.get('r-api/?api=filmes')
+      setfilmes(response.data);
+    }
+
+    loadFilmes();
+
+  }, [])
+
   return (
     <div className="app">
       <h2>Você está na Home</h2>
