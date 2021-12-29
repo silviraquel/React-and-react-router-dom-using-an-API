e4b59ce6df9e4609fde642e4601ede34e8cf6823
Silviane Bezerra <silvianeraquel@gmail.com>
2021-12-29 17:01:52 -0300

    Pagina filmes favoritos salvos 2

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,44 @@
+import {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import './favoritos.css';
+
 export default function Favoritos(){
+    const[filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, []);
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item)=>{
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    }
+
     return(
-        <div>
-            <h1>Página de Filmes Favoritos</h1>
+        <div id='meus-filmes'>
+            <h1>Meus Filmes</h1>
+
+            <ul>
+                {filmes.map((item)=>{
+                  return(
+                    <li key={item.id}>
+                        <span>{item.nome}</span>
+
+                        <div>
+                            <Link to={`/filme/${item.id}`}>Ver detalhes </Link>
+                            <button onClick={() => handleDelete(item.id)}>Excluir</button>
+                        </div>
+                    </li>
+                  )  
+                })}
+            </ul>
         </div>
     )
 }
