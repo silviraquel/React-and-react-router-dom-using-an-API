cc7fb12c837a5521c0b7c56d910db2ca03eb2cf9
Silviane Bezerra <silvianeraquel@gmail.com>
2021-12-29 11:21:36 -0300

    Mostrando página dos detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,17 +1,25 @@
 import {useEffect, useState} from 'react';
 import './filme-info.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 
 export default function Filme(){
     const { id }= useParams();
     const [filme, setFilme] = useState([]);
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
     useEffect(()=>{
 
         async function loadFilmes(){
             const response = await api.get(`r-api/?api=filmes/${id}`)
+
+            if(response.data.length === 0){
+                //tentou  acessar com um ID que não existe, então navega ele para a Home
+                navigate('/');
+                return;
+            }
+
          //   console.log(response.data);
             setFilme(response.data);
             setLoading(false);
@@ -19,7 +27,7 @@ export default function Filme(){
 
         loadFilmes();
 
-    },[id]);
+    },[navigate,id]);
 
     if(loading){
         return(
@@ -31,7 +39,11 @@ export default function Filme(){
 
     return (
         <div className='filme-info'>
-        <h1>PAGINA DETALHES {id} </h1>
+        <h1>{filme.nome}</h1>
+        <img src={filme.foto} alt={filme.nome} />
+
+        <h3>Sinopse</h3>
+        {filme.sinopse}
      </div>
     )        
 }
